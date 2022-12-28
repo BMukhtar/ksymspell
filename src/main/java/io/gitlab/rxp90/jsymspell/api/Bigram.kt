f8a5ea5ed6ec105508d88f8e596c9f205da7e303
Bimurat Mukhtar <v.mukhtar.bimurat@reddit.com>
2022-12-28 18:17:35 +0600

    converted suggest item and bigram

--- src/main/java/io/gitlab/rxp90/jsymspell/api/Bigram.kt
+++ src/main/java/io/gitlab/rxp90/jsymspell/api/Bigram.kt
@@ -1,40 +1,10 @@
-package io.gitlab.rxp90.jsymspell.api;
-
-import java.util.Objects;
+package io.gitlab.rxp90.jsymspell.api
 
 /**
  * Holds a pair of words.
  */
-public class Bigram {
-    private final String word1;
-    private final String word2;
-
-    /**
-     * Constructs a bigram with the specified words.
-     * @param word1 first word
-     * @param word2 second word
-     */
-    public Bigram(String word1, String word2) {
-        this.word1 = word1;
-        this.word2 = word2;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Bigram)) return false;
-        Bigram bigram = (Bigram) o;
-        return Objects.equals(word1, bigram.word1) &&
-                Objects.equals(word2, bigram.word2);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(word1, word2);
-    }
-
-    @Override
-    public String toString() {
-        return word1 + ' ' + word2;
+data class Bigram(private val word1: String, private val word2: String) {
+    override fun toString(): String {
+        return "$word1 $word2"
     }
 }
--- src/main/java/io/gitlab/rxp90/jsymspell/api/SuggestItem.kt
+++ src/main/java/io/gitlab/rxp90/jsymspell/api/SuggestItem.kt
@@ -1,78 +1,26 @@
-package io.gitlab.rxp90.jsymspell.api;
+package io.gitlab.rxp90.jsymspell.api
 
-import java.util.Objects;
+import java.util.*
 
-public final class SuggestItem implements Comparable<SuggestItem> {
-    private final String suggestion;
-    private final int editDistance;
-    private final double frequencyOfSuggestionInDict;
-
-    public SuggestItem(String suggestion, int editDistance, double frequencyOfSuggestionInDict) {
-        this.suggestion = suggestion;
-        this.editDistance = editDistance;
-        this.frequencyOfSuggestionInDict = frequencyOfSuggestionInDict;
-    }
+data class SuggestItem(val suggestion: String, val editDistance: Int, val frequencyOfSuggestionInDict: Double) :
+    Comparable<SuggestItem> {
 
     /**
-     * Compares this {@code SuggestItem} with the specified {@code SuggestItem}.
-     * It will first sort by {@link SuggestItem#getEditDistance()}, and then by {@link SuggestItem#getFrequencyOfSuggestionInDict()}
-     * @param suggestItem {@code SuggestItem} to which this {@code SuggestItem} is to be compared.
-     * @return 0 if this {@code SuggestItem}'s edit distance, and frequency of suggestion are the same as {@code suggestItem}'s
-     *         1 if this {@code SuggestItem}'s edit distance is greater than {@code suggestItem}'s, or if they are equal, this {@code SuggestItem}'s frequency of suggestion is lower
-     *         -1 if this {@code SuggestItem}'s edit distance is lower than {@code suggestItem}'s, or if it's equal and the frequency is greater
+     * Compares this `SuggestItem` with the specified `SuggestItem`.
+     * It will first sort by [SuggestItem.getEditDistance], and then by [SuggestItem.getFrequencyOfSuggestionInDict]
+     *
+     * @param suggestItem `SuggestItem` to which this `SuggestItem` is to be compared.
+     * @return 0 if this `SuggestItem`'s edit distance, and frequency of suggestion are the same as `suggestItem`'s
+     * 1 if this `SuggestItem`'s edit distance is greater than `suggestItem`'s, or if they are equal, this `SuggestItem`'s frequency of suggestion is lower
+     * -1 if this `SuggestItem`'s edit distance is lower than `suggestItem`'s, or if it's equal and the frequency is greater
      */
-    @Override
-    public int compareTo(SuggestItem suggestItem) {
-        if (this.editDistance == suggestItem.editDistance) {
+    override fun compareTo(suggestItem: SuggestItem): Int {
+        return if (editDistance == suggestItem.editDistance) {
             // Descending
-            return Double.compare(suggestItem.frequencyOfSuggestionInDict, frequencyOfSuggestionInDict);
+            suggestItem.frequencyOfSuggestionInDict.compareTo(frequencyOfSuggestionInDict)
         } else {
             // Ascending
-            return Integer.compare(editDistance, suggestItem.editDistance);
+            editDistance.compareTo(suggestItem.editDistance)
         }
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        SuggestItem that = (SuggestItem) o;
-        return editDistance == that.editDistance &&
-                Double.compare(that.frequencyOfSuggestionInDict, frequencyOfSuggestionInDict) == 0 &&
-                Objects.equals(suggestion, that.suggestion);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(suggestion, editDistance, frequencyOfSuggestionInDict);
-    }
-
-    public String getSuggestion() {
-        return suggestion;
-    }
-
-    public int getEditDistance() {
-        return editDistance;
-    }
-
-    public double getFrequencyOfSuggestionInDict() {
-        return frequencyOfSuggestionInDict;
-    }
-
-    @Override
-    public String toString() {
-        return "SuggestItem{"
-                + "suggestion='"
-                + suggestion
-                + '\''
-                + ", editDistance="
-                + editDistance
-                + ", frequencyOfSuggestionInDict="
-                + frequencyOfSuggestionInDict
-                + '}';
     }
 }
