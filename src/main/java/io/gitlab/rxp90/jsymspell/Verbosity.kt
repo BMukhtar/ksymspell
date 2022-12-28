fc060c7d81ea6774917038b7b9c7c6af252f2c7f
Bimurat Mukhtar <v.mukhtar.bimurat@reddit.com>
2022-12-28 18:28:42 +0600

    converted Verbosity

--- src/main/java/io/gitlab/rxp90/jsymspell/Verbosity.kt
+++ src/main/java/io/gitlab/rxp90/jsymspell/Verbosity.kt
@@ -1,19 +1,21 @@
-package io.gitlab.rxp90.jsymspell;
+package io.gitlab.rxp90.jsymspell
 
 /**
  * Allows to control the number of returned results
  */
-public enum Verbosity {
+enum class Verbosity {
     /**
      * Top suggestion with the highest term frequency of the suggestions of smallest edit distance found
      */
     TOP,
+
     /**
      * All suggestions of smallest edit distance found
      */
     CLOSEST,
+
     /**
-     * All suggestions within {@link SymSpell#getMaxDictionaryEditDistance}
+     * All suggestions within [SymSpell.getMaxDictionaryEditDistance]
      */
     ALL
 }
